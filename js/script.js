c005b9548c0e84fea825ef5247ccd3f7fbb771a2
Aisyah Rizqi <aisrizna183@gmail.com>
2025-08-16 09:47:19 +0700

    create style

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
   const welcomeText = document.getElementById("welcome-text");
-  const namePrompt = prompt("Masukkan nama kamu:");
-  if (namePrompt) {
-    welcomeText.textContent = `Hi ${namePrompt}, Welcome To Website`;
+  let namePrompt = prompt("Masukkan nama kamu:");
+  if (namePrompt && namePrompt.trim() !== "") {
+    welcomeText.textContent = `Hi, ${namePrompt.trim()}! Selamat Datang di Website PT. Timah Tbk!`;
+  } else {
+    welcomeText.textContent = "Hi, Selamat Datang di Website PT. Timah Tbk!";
   }
 
-  document.getElementById("current-time").textContent = new Date().toString();
+  document.getElementById("current-time").textContent =
+    new Date().toLocaleString();
 
   document
     .getElementById("message-form")
     .addEventListener("submit", function (e) {
       e.preventDefault();
-
       const name = document.getElementById("name").value;
       const birthdate = document.getElementById("birthdate").value;
       const gender = document.querySelector(
         'input[name="gender"]:checked'
       ).value;
       const message = document.getElementById("message").value;
-
       document.getElementById("res-name").textContent = name;
       document.getElementById("res-birthdate").textContent = birthdate;
       document.getElementById("res-gender").textContent = gender;
